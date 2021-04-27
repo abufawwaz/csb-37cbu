3bbc71ea7268f425e0366300bb951b8381db9da6
Code Sandbox <hello@codesandbox.io>
2021-04-27 05:25:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.51b6243160796956d8813fb8a892b6f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9500aa4f571b9a5dd8f656cb16d6303",
+    "url": "https://abufawwaz.github.io/csb-37cbu/index.html"
+  },
+  {
+    "revision": "3896549a8999c43a542a",
+    "url": "https://abufawwaz.github.io/csb-37cbu/static/js/2.fdf5b1c5.chunk.js"
+  },
+  {
+    "revision": "891c560369175870927c",
+    "url": "https://abufawwaz.github.io/csb-37cbu/static/js/main.adf88fc2.chunk.js"
+  },
+  {
+    "revision": "3618e53cb71c9d742bdf",
+    "url": "https://abufawwaz.github.io/csb-37cbu/static/js/runtime~main.ffb37137.js"
+  }
+]);
